94f5c24845011f2d566b507917eaacdb4af96dd1
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-26 13:29:43 +0200

    adding workspaces model with CRUD methods

---
+++ night-owl-backend/src/models/workspaces.model.ts
@@ -0,0 +1,64 @@
+import database from '../database'
+
+export type Workspace = {
+  id?: string
+  name: string
+}
+
+export class WorkspacesModel {
+  async index(): Promise<Workspace[]> {
+    try {
+      const connect = await database.connect()
+      const sql = `SELECT *
+                   FROM workspaces`
+      const results = await connect.query(sql)
+      connect.release()
+      return results.rows
+    } catch (error) {
+      throw new Error(`Unable to get all workspaces, ${(error as Error).message}`)
+    }
+  }
+
+  async show(id: string): Promise<Workspace> {
+    try {
+      const connect = await database.connect()
+      const sql = `SELECT *
+                   FROM workspaces
+                   WHERE id = $1`
+      const results = await connect.query(sql, [id])
+      connect.release()
+      return results.rows[0]
+    } catch (error) {
+      throw new Error(`Unable to get all workspaces, ${(error as Error).message}`)
+    }
+  }
+
+  async create(workspace: Workspace): Promise<Workspace> {
+    try {
+      const connect = await database.connect()
+      const sql = `INSERT INTO workspaces (name)
+                   VALUES ($1)
+                   RETURNING *`
+      const results = await connect.query(sql, [workspace.name])
+      connect.release()
+      return results.rows[0]
+    } catch (error) {
+      throw new Error(`Unable to get all workspaces, ${(error as Error).message}`)
+    }
+  }
+
+  async delete(id: string): Promise<Workspace> {
+    try {
+      const connect = await database.connect()
+      const sql = `DELETE
+                   FROM workspaces
+                   WHERE id = $1
+                   RETURNING *`
+      const results = await connect.query(sql, [id])
+      connect.release()
+      return results.rows[0]
+    } catch (error) {
+      throw new Error(`Unable to get all workspaces, ${(error as Error).message}`)
+    }
+  }
+}
