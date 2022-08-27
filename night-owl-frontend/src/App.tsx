34978a69844494bc4b558e5267059e939b499304
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-27 19:41:53 +0200

    adding WorkspaceContextProvider Around React app

--- night-owl-frontend/src/App.tsx
+++ night-owl-frontend/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import './Assets/SCSS/home.css'
 import { Routes, Route } from 'react-router-dom'
-import { ChatPage, HomePage, LoginPage, MembersPage, WorkspacePage, JoinWorkspace, CreateWorkspace } from './Pages'
+import { ChatPage, HomePage, LoginPage, MembersPage, WorkspacePage } from './Pages'
 import AuthContextProvider from './Context/auth.context'
 import PrivateRoutes from './Hooks/PrivateRouters'
+import WorkspaceContextProvider from './Context/workspace.context'
 
 function App() {
   return (
     <AuthContextProvider>
-      <Routes>
-        <Route path='/login' element={<LoginPage />} />
-        <Route element={<PrivateRoutes />}>
-          <Route path='/chat' element={<ChatPage />} />
-          <Route path='/home' element={<HomePage />} />
-          <Route path='/members' element={<MembersPage />} />
-          <Route path='/workspace' element={<WorkspacePage />} />
-          <Route path='/workspace/join' element={<JoinWorkspace />} />
-          <Route path='/workspace/create' element={<CreateWorkspace />} />
-        </Route>
-      </Routes>
+      <WorkspaceContextProvider>
+        <Routes>
+          <Route path='/login' element={<LoginPage />} />
+          <Route element={<PrivateRoutes />}>
+            <Route path='/chat' element={<ChatPage />} />
+            <Route path='/home' element={<HomePage />} />
+            <Route path='/members' element={<MembersPage />} />
+            <Route path='/workspace' element={<WorkspacePage />} />
+          </Route>
+        </Routes>
+      </WorkspaceContextProvider>
     </AuthContextProvider>
   )
 }
