93f0ea13e94a1944aaacb94aaff4f1971cae0dda
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-27 19:40:51 +0200

    adding Workspace for export

--- night-owl-frontend/src/Pages/index.ts
+++ night-owl-frontend/src/Pages/index.ts
@@ -3,7 +3,5 @@ import HomePage from './HomePage'
 import LoginPage from './LoginPage'
 import MembersPage from './MembersPage'
 import WorkspacePage from './WorkspacePage'
-import JoinWorkspace from './JoinWorkspace'
-import CreateWorkspace from './CreateWorkspace'
 
-export { ChatPage, HomePage, LoginPage, MembersPage, WorkspacePage, JoinWorkspace, CreateWorkspace }
+export { ChatPage, HomePage, LoginPage, MembersPage, WorkspacePage }
