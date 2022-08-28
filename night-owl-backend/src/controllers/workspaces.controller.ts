cd54c9089be2635986fdcd42ff529690d229b150
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-28 10:15:18 +0200

    deleting creator_id from workspace controller and delete modifying createWorkpace to add role for members, and delete workspace to delete all members for workspace before deleting the workspace

--- night-owl-backend/src/controllers/workspaces.controller.ts
+++ night-owl-backend/src/controllers/workspaces.controller.ts
@@ -4,42 +4,62 @@ import { WorkspaceMembersModel, WorkspacesModel } from '../models'
 const workspacesModel = new WorkspacesModel()
 const workspaceMembersModel = new WorkspaceMembersModel()
 
-export const createWorkspace = async (request: Request, response: Response, next: NextFunction) => {
+export const getAllWorkspaces = async (request: Request, response: Response, next: NextFunction) => {
   try {
-    const workspace = {
-      name: request.body.name,
-      creator: request.body.creator
-    }
-    const newWorkspace = await workspacesModel.create(workspace)
+    const workspaces = await workspacesModel.index()
     response.status(200).json({
       status: 'Success',
-      data: { ...newWorkspace },
+      data: [...workspaces],
+      message: 'All workspaces got retrieved successfully'
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const getWorkspace = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const id = request.params.id
+    const workspace = await workspacesModel.show(id)
+    response.status(200).json({
+      status: 'Success',
+      data: { ...workspace },
+      message: 'Workspace got retrieved successfully'
+    })
+  } catch (error) {
+    next(error)
+  }
+}
+
+export const createWorkspace = async (request: Request, response: Response, next: NextFunction) => {
+  try {
+    const workspaceName = request.body.name
+    const newWorkspace = await workspacesModel.create(workspaceName)
+    const workspaceMember = {
+      workspace_id: newWorkspace.id as string,
+      user_id: request.body.user_id,
+      role: 'Admin'
+    }
+    const workspaceAdmin = await workspaceMembersModel.create(workspaceMember)
+    response.status(200).json({
+      status: 'Success',
+      data: { ...newWorkspace, creator_id: workspaceAdmin.user_id, role: workspaceAdmin.role },
       message: 'Workspace got created successfully'
     })
   } catch (error) {
     next(error)
   }
 }
-export const getWorkspace = async (request: Request, response: Response, next: NextFunction) => {
+
+export const deleteWorkspace = async (request: Request, response: Response, next: NextFunction) => {
   try {
-    
-  } catch (error) {
-   next(error)
-  }
-}
-export const getWorkspaceMembers = async (request: Request, response: Response, next: NextFunction) => {
-  try {
-    const workspaceId = request.params.id
-    const workspace = await workspacesModel.show(workspaceId)
-    const members = await workspaceMembersModel.show(workspaceId)
-    response.status(200).json({
+    const id = request.params.id
+    const deletedWorkspaceMembers = await workspaceMembersModel.deleteWorkspaceMembers(id)
+    const deletedWorkspace = await workspacesModel.delete(id)
+    response.status(202).json({
       status: 'Success',
-      data: {
-        name: workspace.name,
-        creator: { username: workspace.username, email: workspace.email, image: workspace.image },
-        members: [...members]
-      },
-      message: 'All members got retrieved successfully'
+      data: { ...deletedWorkspace, members: [...deletedWorkspaceMembers] },
+      message: 'Workspace got deleted successfully'
     })
   } catch (error) {
     next(error)
