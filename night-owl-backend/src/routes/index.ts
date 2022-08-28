115830866809146526c802f2f4409ed9d9db419e
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-28 10:19:34 +0200

    adding workspaceMember router to express app

--- night-owl-backend/src/routes/index.ts
+++ night-owl-backend/src/routes/index.ts
@@ -1,10 +1,11 @@
 import { Router, Request, Response } from 'express'
-import { usersRoutes, workspacesRoutes } from './api'
+import { usersRoutes, workspacesRoutes, workspaceMembersRoutes } from './api'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/workspaces', workspacesRoutes)
+routes.use('/workspace/members', workspaceMembersRoutes)
 
 routes.get('/api', (req: Request, res: Response) => {
   res.send('Hello from api')
