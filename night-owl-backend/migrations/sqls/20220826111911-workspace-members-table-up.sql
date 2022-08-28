b2f57e8dbef0560ac6abbdf2dbc3c73b7aa5de14
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-28 10:13:07 +0200

    adding role to workspace_members migration

--- night-owl-backend/migrations/sqls/20220826111911-workspace-members-table-up.sql
+++ night-owl-backend/migrations/sqls/20220826111911-workspace-members-table-up.sql
@@ -1,6 +1,7 @@
 CREATE TABLE workspace_members
 (
     id           SERIAL PRIMARY KEY,
-    workspace_id BIGINT REFERENCES workspaces (id),
-    user_id      uuid REFERENCES users (id)
+    workspace_id uuid REFERENCES workspaces (id) NOT NULL,
+    user_id      uuid REFERENCES users (id)      NOT NULL UNIQUE,
+    role         VARCHAR                         NOT NULL
 );
