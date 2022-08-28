888aff5f8bffb3aa49092d71f21901ef03050743
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-28 10:20:10 +0200

    adding workspaceMembersRoutes for export in api index file

--- night-owl-backend/src/routes/api/index.ts
+++ night-owl-backend/src/routes/api/index.ts
@@ -1,4 +1,5 @@
 import usersRoutes from './users.routes'
 import workspacesRoutes from './workspaces.routes'
+import workspaceMembersRoutes from './workspace_members.routes'
 
-export { usersRoutes, workspacesRoutes }
+export { usersRoutes, workspacesRoutes, workspaceMembersRoutes }
