605b66fa9c09ba8026f12845ccb65c59757931c0
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-29 07:53:38 +0200

    adding workspace request to router

--- night-owl-backend/src/routes/index.ts
+++ night-owl-backend/src/routes/index.ts
@@ -1,11 +1,12 @@
 import { Router, Request, Response } from 'express'
-import { usersRoutes, workspacesRoutes, workspaceMembersRoutes } from './api'
+import { usersRoutes, workspacesRoutes, workspaceMembersRoutes, workspaceRequestsRoutes } from './api'
 
 const routes = Router()
 
 routes.use('/users', usersRoutes)
 routes.use('/workspaces', workspacesRoutes)
 routes.use('/workspace/members', workspaceMembersRoutes)
+routes.use('/workspace/requests', workspaceRequestsRoutes)
 
 routes.get('/api', (req: Request, res: Response) => {
   res.send('Hello from api')
