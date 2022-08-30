e7aeba985944013651020ab0e1ee8f644f64ef39
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-30 15:42:11 +0200

    moving auth user, workspace and workspace request types to global  Types folder

---
+++ night-owl-frontend/src/Types/index.ts
@@ -0,0 +1,20 @@
+export type AuthUser = {
+  id: string
+  username: string
+  email: string
+  image: string
+  is_verified: boolean
+  token: string
+}
+
+export type Workspace = {
+  id: string
+  name: string
+}
+
+export type WorkspaceRequest = {
+  id: string
+  name: string
+  workspace_id: string
+  state: string
+}
