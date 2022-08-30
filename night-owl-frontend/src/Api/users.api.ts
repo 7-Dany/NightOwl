02962b332a45602e1ce58d0b1cd637acbd0ac4f4
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-30 15:46:43 +0200

    adding delete user session api call for logout

--- night-owl-frontend/src/Api/users.api.ts
+++ night-owl-frontend/src/Api/users.api.ts
@@ -1,13 +1,25 @@
 import axios from 'axios'
 import config from '../Config'
+import { AuthUser, Workspace, WorkspaceRequest } from '../Types'
 
 const { url } = config
+
+export type UserWithWorkspace = {
+  user: AuthUser
+  workspace?: Workspace
+  workspaceRequest: WorkspaceRequest
+}
 
 type GetUserArgs = {
   controller: AbortController
 }
 
-export async function getUser({ controller }: GetUserArgs) {
+type DeleteUserSessionArgs = {
+  controller: AbortController
+  token: string
+}
+
+export async function getUser({ controller }: GetUserArgs): Promise<UserWithWorkspace> {
   const config = {
     headers: { 'Content-Type': 'application/json' },
     withCredentials: true,
@@ -15,4 +27,14 @@ export async function getUser({ controller }: GetUserArgs) {
   }
   const response = await axios.get(`${url}/users/auth/session`, config)
   return response.data.data
+}
+
+export async function deleteUserSession({ controller, token }: DeleteUserSessionArgs): Promise<void> {
+  const config = {
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${token}` },
+    withCredentials: true,
+    signal: controller.signal
+  }
+  const response = await axios.get(`${url}/users/auth/logout`, config)
+  return response.data
 }
