51a4d4db2d7898a9a6cc1871faaf89345cb21a92
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-30 14:56:11 +0200

    making user id unique in workspace_requests

--- night-owl-backend/migrations/sqls/20220828154730-workspace-requests-up.sql
+++ night-owl-backend/migrations/sqls/20220828154730-workspace-requests-up.sql
@@ -1,7 +1,7 @@
 CREATE TABLE workspace_requests
 (
     id           SERIAL PRIMARY KEY,
-    user_id      uuid REFERENCES users (id),
+    user_id      uuid REFERENCES users (id) UNIQUE,
     workspace_id uuid REFERENCES workspaces (id),
     state        VARCHAR NOT NULL
 );
