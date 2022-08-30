dd4719c695f4d8dd2c0a5abe3afef44d8263a0ba
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-30 15:43:29 +0200

    changing component name and moving JSX elements to workspace member component

--- night-owl-frontend/src/Features/Members/index.tsx
+++ night-owl-frontend/src/Features/Members/index.tsx
@@ -1,29 +1,9 @@
-import Member from './Components/Member'
+import WorkspaceMembers from './Components/WorkspaceMembers'
 
-function Members() {
+function WorkspaceMembersMain() {
   return (
-    <div className='members-container'>
-      <div className='members-title'>
-        <h2>Workspace Members</h2>
-        <p>There is 1 active Member</p>
-      </div>
-      <div className='members-header'>
-        <span className='members-header__image'>Image</span>
-        <span className='members-header__name'>Name</span>
-        <span className='members-header__email'>Email</span>
-        <span className='members-header__projects'>Projects</span>
-        <span className='members-header__role'>Role</span>
-        <span className='members-header__timezone'>Timezone</span>
-        <span className='members-header__chat'>Chat</span>
-      </div>
-      <div className='members'>
-        <Member email={'ali@gmail.com'} name={'Ali'} image={'./images/person.svg'} role={'Admin'}
-                projects={2} timezone={'GMT+3'} />
-        <Member email={'speedleopard2018@gmail.com'} name={'Ali'} image={'./images/person.svg'} role={'Admin'}
-                projects={2} timezone={'GMT+3'} />
-      </div>
-    </div>
+    <WorkspaceMembers />
   )
 }
 
-export default Members
+export default WorkspaceMembersMain
