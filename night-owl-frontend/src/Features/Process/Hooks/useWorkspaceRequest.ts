9416731d55ba46f9ef714fd9804ef66baad342cb
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-08-30 16:19:42 +0200

    moving hooks and function from workspace request component

---
+++ night-owl-frontend/src/Features/Process/Hooks/useWorkspaceRequest.ts
@@ -0,0 +1,53 @@
+import React, { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../../Context/auth.context'
+import { useNavigate } from 'react-router-dom'
+import { deleteWorkspaceRequest } from '../Api/workspaces.api'
+import { AuthUser, WorkspaceRequest } from '../../../Types'
+
+function useWorkspaceRequest() {
+  const { workspaceRequest, setWorkspaceRequest, user, setUser } = useContext(AuthContext)
+  const [deleteRequest, setDeleteRequest] = useState<boolean | null>(null)
+  const navigate = useNavigate()
+  const [errorMsg, setErrorMsg] = useState('')
+
+  useEffect(() => {
+    const controller = new AbortController()
+    if (deleteRequest) {
+      deleteWorkspaceRequest({ controller, values: { id: workspaceRequest.id, token: user.token } })
+        .then(data => {
+          setErrorMsg('Your request got deleted')
+          setWorkspaceRequest({} as WorkspaceRequest)
+          setDeleteRequest(null)
+          navigate('/workspace')
+        })
+        .catch(error => {
+          setErrorMsg(error.response.data.message)
+          setWorkspaceRequest({} as WorkspaceRequest)
+          setDeleteRequest(null)
+        })
+    }
+    return () => {
+      controller.abort()
+    }
+  }, [deleteRequest])
+
+  function confirmOption(event: React.MouseEvent<HTMLButtonElement>, order: boolean | null) {
+    setDeleteRequest(order)
+  }
+
+  function closeWorkspaceRequest() {
+    setWorkspaceRequest({} as WorkspaceRequest)
+    setUser({} as AuthUser)
+    navigate('/login')
+  }
+
+  return {
+    workspaceRequest,
+    deleteRequest,
+    errorMsg,
+    closeWorkspaceRequest,
+    confirmOption
+  }
+}
+
+export default useWorkspaceRequest
