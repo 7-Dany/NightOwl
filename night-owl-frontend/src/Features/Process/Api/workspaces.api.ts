93c7639eed68d1173c8dc33328ea8238a041c26f
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-01 20:34:30 +0200

    moving workspaceRequests calls to workspace_requests file

--- night-owl-frontend/src/Features/Process/Api/workspaces.api.ts
+++ night-owl-frontend/src/Features/Process/Api/workspaces.api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import config from '../../../Config'
-import { WorkspaceRequest } from '../../../Types'
+import { Workspace } from '../../../Types'
 
 const { url } = config
 
@@ -13,60 +13,12 @@ type CreateWorkspaceArgs = {
   }
 }
 
-type CreateWorkspaceReturn = {
-  id: string
-  name: string
-}
-
-type CreateWorkspaceRequestArgs = {
-  controller: AbortController
-  values: {
-    workspace_id: string
-    user_id: string
-    token: string
-  }
-}
-
-type DeleteWorkspaceRequestsArgs = {
-  controller: AbortController
-  values: {
-    id: string
-    token: string
-  }
-}
-
-export async function createWorkspace({ controller, values }: CreateWorkspaceArgs): Promise<CreateWorkspaceReturn> {
+export async function createWorkspace({ controller, values }: CreateWorkspaceArgs): Promise<Workspace> {
   const config = {
     headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${values.token}` },
     withCredentials: true,
     signal: controller.signal
   }
   const response = await axios.post(`${url}/workspaces`, { ...values }, config)
-  return response.data.data
-}
-
-export async function createWorkspaceRequest({
-                                               controller,
-                                               values
-                                             }: CreateWorkspaceRequestArgs): Promise<WorkspaceRequest> {
-  const config = {
-    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${values.token}` },
-    withCredentials: true,
-    signal: controller.signal
-  }
-  const response = await axios.post(`${url}/workspace/requests`, { ...values }, config)
-  return response.data.data
-}
-
-export async function deleteWorkspaceRequest({
-                                               controller,
-                                               values
-                                             }: DeleteWorkspaceRequestsArgs): Promise<WorkspaceRequest> {
-  const config = {
-    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${values.token}` },
-    withCredentials: true,
-    signal: controller.signal
-  }
-  const response = await axios.delete(`${url}/workspace/requests/${values.id}`, config)
   return response.data.data
 }
