4e1e68075b3fa6e74791306e3fd6d5bc31fb63df
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-04 19:20:40 +0200

    deleting state from workspace_requests schema

--- night-owl-backend/migrations/sqls/20220828154730-workspace-requests-up.sql
+++ night-owl-backend/migrations/sqls/20220828154730-workspace-requests-up.sql
@@ -2,6 +2,5 @@ CREATE TABLE workspace_requests
 (
     id           SERIAL PRIMARY KEY,
     user_id      uuid REFERENCES users (id) UNIQUE,
-    workspace_id uuid REFERENCES workspaces (id),
-    state        VARCHAR NOT NULL
+    workspace_id uuid REFERENCES workspaces (id)
 );
