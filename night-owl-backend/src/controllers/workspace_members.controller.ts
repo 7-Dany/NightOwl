c3016f8d94796bf7b1336870903ec0c2dac69675
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-04 19:50:29 +0200

    adding check request if the user has request or not if he has it should delete the request

--- night-owl-backend/src/controllers/workspace_members.controller.ts
+++ night-owl-backend/src/controllers/workspace_members.controller.ts
@@ -1,7 +1,8 @@
 import { Request, Response, NextFunction } from 'express'
-import { WorkspaceMembersModel } from '../models'
+import { WorkspaceMembersModel, WorkspaceRequestsModel } from '../models'
 
 const workspaceMembersModel = new WorkspaceMembersModel()
+const workspaceRequestsModel = new WorkspaceRequestsModel()
 
 export const getAllWorkspaceMembers = async (request: Request, response: Response, next: NextFunction) => {
   try {
@@ -19,7 +20,7 @@ export const getAllWorkspaceMembers = async (request: Request, response: Respons
 export const getWorkspaceMembers = async (request: Request, response: Response, next: NextFunction) => {
   try {
     const id = request.params.id
-    const workspaceMembers = await workspaceMembersModel.show(id)
+    const workspaceMembers = await workspaceMembersModel.showByWorkspaceId(id)
     response.status(200).json({
       status: 'Success',
       data: [...workspaceMembers],
@@ -38,6 +39,10 @@ export const createWorkspaceMember = async (request: Request, response: Response
       role: request.body.role
     }
     const newWorkspaceMember = await workspaceMembersModel.create(workspaceMember)
+    const checkRequest = await workspaceRequestsModel.showByUserId(workspaceMember.user_id)
+    if (checkRequest) {
+      await workspaceRequestsModel.deleteByUserId(workspaceMember.user_id)
+    }
     response.status(201).json({
       status: 'Success',
       body: { ...newWorkspaceMember },
