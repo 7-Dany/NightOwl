bc9e431d5fc3264f92b3d335c2c58451079967c5
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-04 19:32:21 +0200

    using useRequest hook, add functionality for delete and accept buttons

--- night-owl-frontend/src/Features/Members/Components/Request.tsx
+++ night-owl-frontend/src/Features/Members/Components/Request.tsx
@@ -1,17 +1,40 @@
+import useRequest from '../Hooks/useRequest'
+
 type RequestProps = {
+  userId: string
   image: string
   name: string
   email: string
 }
 
-function Request({ image, name, email }: RequestProps) {
+function Request({ userId, image, name, email }: RequestProps) {
+  const { msg, requestOrder } = useRequest({ userId })
   return (
-    <div className='request'>
-      <img src={image} alt='person' className='request__image' />
-      <h3 className='request__name'>{name}</h3>
-      <p className='request__email'>{email}</p>
-      <button className='request__accept-btn'>Accept</button>
-      <button className='request__delete-btn'>Delete</button>
+    <div className='request-container'>
+      <div className='request'>
+        <img src={image} alt='person' className='request__image' />
+        <h3 className='request__name'>{name}</h3>
+        <p className='request__email'>{email}</p>
+      </div>
+      <div className='request-choice'>
+        {msg &&
+          <p className='request-choice__text'>{msg}</p>
+        }
+        {!msg &&
+          <button className='request-choice__accept-btn'
+                  onClick={(event) => requestOrder(event, 'Accept')}
+          >
+            Accept
+          </button>
+        }
+        {!msg &&
+          <button className='request-choice__delete-btn'
+                  onClick={(event) => requestOrder(event, 'Delete')}
+          >
+            Delete
+          </button>
+        }
+      </div>
     </div>
   )
 }
