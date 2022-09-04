1af4fbc7859ae8308039e95f8cb16354a8fc6b90
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-04 19:49:51 +0200

    adding showRequestsOrMembers function to show requests or members

--- night-owl-frontend/src/Features/Members/Components/WorkspaceMembers.tsx
+++ night-owl-frontend/src/Features/Members/Components/WorkspaceMembers.tsx
@@ -1,11 +1,15 @@
 import Member from './Member'
 import { WorkspaceMember } from '../Types'
+import { LeftArrowIcon } from '../Assets'
+import React from 'react'
 
 type WorkspaceMembersProps = {
   members: WorkspaceMember[]
+  showRequestsOrMembers: (event: React.MouseEvent<HTMLDivElement>, show: string) => void
 }
 
-function WorkspaceMembers({ members }: WorkspaceMembersProps) {
+function WorkspaceMembers({ members, showRequestsOrMembers }: WorkspaceMembersProps) {
+
   const workspaceMembers = members.map(member => {
     return (
       <Member
@@ -20,11 +24,16 @@ function WorkspaceMembers({ members }: WorkspaceMembersProps) {
       />
     )
   })
+
   return (
     <div className='members-container'>
+      <div className='members-back' onClick={(event) => showRequestsOrMembers(event, 'main')}>
+        <LeftArrowIcon className={'members-back__icon'} />
+        <span className='members-back__text'>Back</span>
+      </div>
       <div className='members-title'>
-        <h2>Workspace Members</h2>
-        <p>There is 1 active Member</p>
+        <h2>Members</h2>
+        <p>There {members.length > 1 ? 'are' : 'is'} {members.length} active Member{members.length > 1 ? 's' : ''}</p>
       </div>
       <div className='members-header'>
         <span className='members-header__image'>Image</span>
