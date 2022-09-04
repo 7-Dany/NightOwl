5231204da08887dd1388074bbe0c28280bb24115
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-04 19:48:01 +0200

    adding api call to create workspace member

---
+++ night-owl-frontend/src/Features/Members/Api/workspace_members.ts
@@ -0,0 +1,31 @@
+import config from '../../../Config'
+import axios from 'axios'
+import { WorkspaceMember } from '../Types'
+
+const { url } = config
+
+type CreateWorkspaceMemberArgs = {
+  controller: AbortController
+  values: {
+    workspaceId: string
+    token: string
+    role: string
+    userId: string
+  }
+}
+
+export async function createWorkspaceMember({
+                                              controller,
+                                              values
+                                            }: CreateWorkspaceMemberArgs): Promise<WorkspaceMember> {
+  const config = {
+    headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${values.token}` },
+    withCredentials: true,
+    signal: controller.signal
+  }
+  const response = await axios.post(`${url}/workspace/members/${values.workspaceId}`, {
+    role: values.role,
+    user_id: values.userId
+  }, config)
+  return response.data.data
+}
