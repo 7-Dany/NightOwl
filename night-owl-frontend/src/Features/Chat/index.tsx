9cf50ce935f1cbfc6fcd2357d920e77c9fdcd025
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-06 23:07:14 +0200

    Adding ChatContextProvider

--- night-owl-frontend/src/Features/Chat/index.tsx
+++ night-owl-frontend/src/Features/Chat/index.tsx
@@ -1,20 +1,21 @@
 import { TeamChannelsContainer, PrivateChannelsContainer, ChatRoomHeader, ChatRoom } from './Components'
-import useSocket from './Hooks/useSocket'
+import ChatContextProvider from './Context/chat.context'
 
 function WorkspaceChatMain() {
-  useSocket()
   return (
-    <div className='chats-container'>
-      <div className='chats'>
-        <h2>Chats</h2>
-        <TeamChannelsContainer />
-        <PrivateChannelsContainer />
+    <ChatContextProvider>
+      <div className='chats-container'>
+        <div className='chats'>
+          <h2>Chats</h2>
+          <TeamChannelsContainer />
+          <PrivateChannelsContainer />
+        </div>
+        <div className='chat-room-container'>
+          <ChatRoomHeader />
+          <ChatRoom />
+        </div>
       </div>
-      <div className='chat-room-container'>
-        <ChatRoomHeader />
-        <ChatRoom />
-      </div>
-    </div>
+    </ChatContextProvider>
   )
 }
 
