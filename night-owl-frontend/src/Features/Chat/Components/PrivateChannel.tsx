023b7c5752e926a2c6fbb089762a8f76af6e1db6
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-06 23:08:21 +0200

    adding props for PrivateChannel component

--- night-owl-frontend/src/Features/Chat/Components/PrivateChannel.tsx
+++ night-owl-frontend/src/Features/Chat/Components/PrivateChannel.tsx
@@ -1,16 +1,25 @@
-import { PersonImage } from '../../../Assets'
+import { Conversation } from '../Types'
 
-function PrivateChannel() {
+
+function PrivateChannel({
+                          user_id,
+                          email,
+                          image,
+                          text,
+                          username,
+                          conversation_id,
+                          created_at
+                        }: Conversation) {
+
   return (
     <div className='private-channel'>
       <div className='private-channel__name-and-image'>
-        <img src={PersonImage} alt='person' />
-        <p className='private-channel__title'>Person Name</p>
-        <p className='private-channel__last-msg'>How are you?</p>
+        <img src={image} alt='person' />
+        <p className='private-channel__title'>{username}</p>
+        <p className='private-channel__last-msg'>{text}</p>
       </div>
       <div className='private-channel__unseen-msg'>
-        <span className='private-channel__last-msg-date'>4:45 pm</span>
-        <span className='private-channel__counts'>5</span>
+        <span className='private-channel__last-msg-date'>{created_at}</span>
       </div>
     </div>
   )
