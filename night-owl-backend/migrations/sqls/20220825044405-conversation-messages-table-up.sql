d7e881f1d244850d6efb788720ab0f1795d0559f
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-06 23:00:07 +0200

    deleting conversation_messages table

--- night-owl-backend/migrations/sqls/20220825044405-conversation-messages-table-up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE conversation_messages
-(
-    id              SERIAL PRIMARY KEY,
-    conversation_id uuid REFERENCES conversations (id) NOT NULL,
-    message_id      BIGINT REFERENCES messages (id)   NOT NULL
-);
