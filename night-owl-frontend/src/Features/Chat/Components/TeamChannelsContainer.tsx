55c8c1fac55996b0ec78b193af6e6871de56fbf9
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-10 01:29:38 +0200

    added function to show all team channels or hide them

--- night-owl-frontend/src/Features/Chat/Components/TeamChannelsContainer.tsx
+++ night-owl-frontend/src/Features/Chat/Components/TeamChannelsContainer.tsx
@@ -1,18 +1,28 @@
 import { ArrowIcon } from '../../../Assets'
 import TeamChannel from './TeamChannel'
+import React, { useState } from 'react'
 
 function TeamChannelsContainer() {
+  const [showTeamChannels, setShowTeamChannels] = useState(true)
+
+  function handleShowTeamChannels(event: React.MouseEvent<HTMLDivElement>) {
+    setShowTeamChannels(prevShowTeamChannels => !prevShowTeamChannels)
+  }
+
   return (
     <div className='team-channels-container'>
       <div className='team-channels'>
         <p className='team-channels__text'>Channels</p>
-        <div className='team-channels__icon-container'>
+        <div className={showTeamChannels ? 'team-channels__icon-container' : 'team-channels__icon-container active'}
+             onClick={handleShowTeamChannels}>
           <ArrowIcon className={'team-channels__icon'} />
         </div>
       </div>
-      <div className='channels-container'>
-        <TeamChannel />
-      </div>
+      {showTeamChannels &&
+        <div className='channels-container'>
+          <TeamChannel />
+        </div>
+      }
     </div>
   )
 }
