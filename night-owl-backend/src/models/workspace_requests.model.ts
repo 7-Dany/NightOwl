2a7ccf597e0d909930ba9b7994d105e46bfcd476
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-10 01:35:52 +0200

    added show by id, delete by id and delete by workspace id methods

--- night-owl-backend/src/models/workspace_requests.model.ts
+++ night-owl-backend/src/models/workspace_requests.model.ts
@@ -20,6 +20,20 @@ export class WorkspaceRequestsModel {
     }
   }
 
+  async show(id: string): Promise<WorkspaceRequest> {
+    try {
+      const connect = await database.connect()
+      const sql = `SELECT *
+                   FROM workspace_requests
+                   WHERE id = $1`
+      const results = await connect.query(sql, [id])
+      connect.release()
+      return results.rows[0]
+    } catch (error) {
+      throw new Error(`Unable to show workspace request by id, ${(error as Error).message}`)
+    }
+  }
+
   async showByWorkspaceId(workspaceId: string): Promise<WorkspaceRequest[]> {
     try {
       const connect = await database.connect()
@@ -31,7 +45,7 @@ export class WorkspaceRequestsModel {
       connect.release()
       return results.rows
     } catch (error) {
-      throw new Error(`Unable to get workspace request, ${(error as Error).message}`)
+      throw new Error(`Unable to show workspace request by workspace id, ${(error as Error).message}`)
     }
   }
 
@@ -46,7 +60,7 @@ export class WorkspaceRequestsModel {
       connect.release()
       return results.rows[0]
     } catch (error) {
-      throw new Error(`Unable to get workspace request by user id, ${(error as Error).message}`)
+      throw new Error(`Unable to show workspace request by user id, ${(error as Error).message}`)
     }
   }
 
@@ -64,6 +78,21 @@ export class WorkspaceRequestsModel {
     }
   }
 
+  async delete(id: string): Promise<WorkspaceRequest> {
+    try {
+      const connect = await database.connect()
+      const sql = `DELETE
+                   FROM workspace_requests
+                   WHERE id = $1
+                   RETURNING *`
+      const results = await connect.query(sql, [id])
+      connect.release()
+      return results.rows[0]
+    } catch (error) {
+      throw new Error(`Unable to delete workspace request by id, ${(error as Error).message}`)
+    }
+  }
+
   async deleteByUserId(userId: string): Promise<WorkspaceRequest> {
     try {
       const connect = await database.connect()
@@ -75,7 +104,22 @@ export class WorkspaceRequestsModel {
       connect.release()
       return results.rows[0]
     } catch (error) {
-      throw new Error(`Unable to delete workspace request, ${(error as Error).message}`)
+      throw new Error(`Unable to delete workspace request by user id, ${(error as Error).message}`)
+    }
+  }
+
+  async deleteByWorkspaceId(workspaceId: string): Promise<WorkspaceRequest[]> {
+    try {
+      const connect = await database.connect()
+      const sql = `DELETE
+                   FROM workspace_requests
+                   WHERE workspace_id = $1
+                   RETURNING *`
+      const results = await connect.query(sql, [workspaceId])
+      connect.release()
+      return results.rows
+    } catch (error) {
+      throw new Error(`Unable to delete workspace requests by workspace id, ${(error as Error).message}`)
     }
   }
 }
