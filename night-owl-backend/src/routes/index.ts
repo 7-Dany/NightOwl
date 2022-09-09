2cbae610ed0995d1ab3a0221518eaf21064ef6a3
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-10 01:24:58 +0200

    adding conversations and conversationMembers for routes

--- night-owl-backend/src/routes/index.ts
+++ night-owl-backend/src/routes/index.ts
@@ -1,5 +1,12 @@
 import { Router, Request, Response } from 'express'
-import { usersRoutes, workspacesRoutes, workspaceMembersRoutes, workspaceRequestsRoutes } from './api'
+import {
+  usersRoutes,
+  workspacesRoutes,
+  workspaceMembersRoutes,
+  workspaceRequestsRoutes,
+  conversationMembersRoutes,
+  conversationsRoutes
+} from './api'
 
 const routes = Router()
 
@@ -7,6 +14,8 @@ routes.use('/api', usersRoutes)
 routes.use('/api', workspacesRoutes)
 routes.use('/api', workspaceMembersRoutes)
 routes.use('/api', workspaceRequestsRoutes)
+routes.use('/api', conversationsRoutes)
+routes.use('/api', conversationMembersRoutes)
 
 routes.get('/api', (req: Request, res: Response) => {
   res.send('Hello from api')
