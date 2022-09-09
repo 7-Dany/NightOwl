1daec9afdb8fffda1b14187064a716a61cffd410
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-10 01:29:10 +0200

    added function to order the conversations and move active conversation to top

--- night-owl-frontend/src/Features/Chat/Components/PrivateChannelsContainer.tsx
+++ night-owl-frontend/src/Features/Chat/Components/PrivateChannelsContainer.tsx
@@ -1,28 +1,55 @@
 import { ArrowIcon } from '../../../Assets'
 import PrivateChannel from './PrivateChannel'
-import { useContext } from 'react'
-import { ChatContext } from '../Context/chat.context'
+import { Conversation } from '../Types'
+import React, { useContext, useState } from 'react'
+import { ActiveContext } from '../../../Context/active.context'
 
-function PrivateChannelsContainer() {
-  const { userConversations } = useContext(ChatContext)
+type PrivateChannelsContainerProps = {
+  userConversations: Conversation[]
+}
 
-  const conversations = userConversations.map(conversation => {
-    return (
-      <PrivateChannel key={conversation.conversation_id} {...conversation} />
-    )
-  })
+function PrivateChannelsContainer({ userConversations }: PrivateChannelsContainerProps) {
+  const [showPrivateChannels, setShowPrivateChannels] = useState(true)
+  const { activeConversation } = useContext(ActiveContext)
+  let conversations
+  if (activeConversation.conversation_id) {
+    let chosenConversation
+    const filteredConversations = userConversations.map(conversation => {
+      if (conversation.conversation_id === activeConversation.conversation_id) {
+        chosenConversation = <PrivateChannel key={conversation.conversation_id} {...conversation} />
+      } else {
+        return <PrivateChannel key={conversation.conversation_id} {...conversation} />
+      }
+    })
+    conversations = [chosenConversation, ...filteredConversations]
+  } else {
+    conversations = userConversations.map(conversation => {
+      return (
+        <PrivateChannel key={conversation.conversation_id} {...conversation} />
+      )
+    })
+  }
+
+  function handleShowPrivateChannels(event: React.MouseEvent<HTMLDivElement>) {
+    setShowPrivateChannels(prevShowPrivateChannels => !prevShowPrivateChannels)
+  }
 
   return userConversations ? (
     <div className='private-channels-container'>
       <div className='private-channels'>
         <p className='private-channels__text'>Direct messages</p>
-        <div className='private-channels__icon-container'>
+        <div
+          className={showPrivateChannels ? 'private-channels__icon-container' : 'private-channels__icon-container active'}
+          onClick={handleShowPrivateChannels}
+        >
           <ArrowIcon className={'private-channels__icon'} />
         </div>
       </div>
-      <div className='all-private-channels-container'>
-        {conversations}
-      </div>
+      {showPrivateChannels &&
+        <div className='all-private-channels-container'>
+          {conversations}
+        </div>
+      }
     </div>
   ) : <div></div>
 }
