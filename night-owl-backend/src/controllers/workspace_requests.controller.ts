3b348820a82c185b43b14f9ca77cbbcbc012d744
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-10 01:35:19 +0200

    added deleteWorkspaceRequest by id and updated deleteWorkspaceRequestByUser

--- night-owl-backend/src/controllers/workspace_requests.controller.ts
+++ night-owl-backend/src/controllers/workspace_requests.controller.ts
@@ -1,11 +1,15 @@
-import { WorkspaceRequestsModel, WorkspacesModel, WorkspaceMembersModel } from '../models'
+import { WorkspaceRequestsModel, WorkspacesModel } from '../models'
 import { Request, Response, NextFunction } from 'express'
 import { StatusError } from '../middlewares/error.middleware'
 
 const workspaceRequestsModel = new WorkspaceRequestsModel()
 const workspacesModel = new WorkspacesModel()
 
-export const getAllWorkspaceRequests = async (request: Request, response: Response, next: NextFunction) => {
+export const getAllWorkspaceRequests = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const workspaceRequests = await workspaceRequestsModel.index()
     response.status(200).json({
@@ -17,10 +21,15 @@ export const getAllWorkspaceRequests = async (request: Request, response: Respon
     next(error)
   }
 }
-export const getWorkspaceRequest = async (request: Request, response: Response, next: NextFunction) => {
+
+export const getWorkspaceRequest = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id = request.params.id
-    const workspaceRequest = await workspaceRequestsModel.showByWorkspaceId(id)
+    const workspaceRequest = await workspaceRequestsModel.show(id)
     response.status(200).json({
       status: 'Success',
       data: { ...workspaceRequest },
@@ -31,15 +40,17 @@ export const getWorkspaceRequest = async (request: Request, response: Response, 
   }
 }
 
-export const createWorkspaceRequest = async (request: Request, response: Response, next: NextFunction) => {
+export const createWorkspaceRequest = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
-    const workspaceRequest = {
-      user_id: request.body.user_id,
-      workspace_id: request.body.workspace_id
-    }
-    const checkWorkspace = await workspacesModel.show(workspaceRequest.workspace_id)
+    const user_id = request.body.user_id
+    const workspace_id = request.body.workspace_id
+    const checkWorkspace = await workspacesModel.show(workspace_id)
     if (checkWorkspace) {
-      const newWorkspaceRequest = await workspaceRequestsModel.create(workspaceRequest)
+      const newWorkspaceRequest = await workspaceRequestsModel.create({ user_id, workspace_id })
       response.status(201).json({
         status: 'Success',
         data: { name: checkWorkspace.name, ...newWorkspaceRequest },
@@ -47,7 +58,8 @@ export const createWorkspaceRequest = async (request: Request, response: Respons
       })
     } else {
       response.status(422).json({
-        status: 'Failed', message: 'Workspace is not exist make sure to write a correct id'
+        status: 'Failed',
+        message: 'Workspace is not exist make sure to write a correct id'
       })
     }
   } catch (error) {
@@ -56,16 +68,52 @@ export const createWorkspaceRequest = async (request: Request, response: Respons
     next(newError)
   }
 }
-
-export const deleteWorkspaceRequest = async (request: Request, response: Response, next: NextFunction) => {
+export const deleteWorkspaceRequest = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
   try {
     const id = request.params.id
-    const deletedWorkspaceRequest = await workspaceRequestsModel.deleteByUserId(id)
-    response.status(202).json({
-      status: 'Success',
-      data: { ...deletedWorkspaceRequest },
-      message: 'Workspace request got deleted successfully'
-    })
+    const checkWorkspaceRequest = await workspaceRequestsModel.show(id)
+    if (checkWorkspaceRequest) {
+      const deletedWorkspaceRequest = await workspaceRequestsModel.delete(id)
+      response.status(202).json({
+        status: 'Success',
+        data: { ...deletedWorkspaceRequest },
+        message: 'Workspace Request got deleted successfully'
+      })
+    } else {
+      response.status(422).json({
+        status: 'Failed',
+        message: 'Workspace request is not exist'
+      })
+    }
+  } catch (error) {
+
+  }
+}
+export const deleteWorkspaceRequestByUser = async (
+  request: Request,
+  response: Response,
+  next: NextFunction
+): Promise<void> => {
+  try {
+    const userId = request.params.user_id
+    const checkWorkspaceRequest = await workspaceRequestsModel.showByUserId(userId)
+    if (checkWorkspaceRequest) {
+      const deletedWorkspaceRequest = await workspaceRequestsModel.deleteByUserId(userId)
+      response.status(202).json({
+        status: 'Success',
+        data: { ...deletedWorkspaceRequest },
+        message: 'Workspace request got deleted successfully'
+      })
+    } else {
+      response.status(422).json({
+        status: 'Failed',
+        message: 'Workspace request is not exist'
+      })
+    }
   } catch (error) {
     next(error)
   }
