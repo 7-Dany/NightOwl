0586adfa4ec0d40c87a1590eb8c7fd0302391323
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-10 01:19:12 +0200

    adding optional render to render chat members if its team chat

--- night-owl-frontend/src/Features/Chat/Components/ChatRoomHeader.tsx
+++ night-owl-frontend/src/Features/Chat/Components/ChatRoomHeader.tsx
@@ -1,18 +1,23 @@
 import { DotsIcons, PersonImage } from '../../../Assets'
+import { useContext } from 'react'
+import { ActiveContext } from '../../../Context/active.context'
 
 function ChatRoomHeader() {
+  const { activeConversation } = useContext(ActiveContext)
   return (
     <div className='chat-room-header'>
       <div className='chat-room-header__info'>
-        <img src={PersonImage} alt='Chat' />
-        <p>Book App</p>
+        <img src={activeConversation.image} alt='Chat' />
+        <p>{activeConversation.type === 'Private' ? activeConversation.username : activeConversation.name}</p>
       </div>
-      <div className='chat-room-header__members'>
-        <img src={PersonImage} alt='Person' />
-        <img src={PersonImage} alt='Person' />
-        <img src={PersonImage} alt='Person' />
-        <span>+24</span>
-      </div>
+      {activeConversation.type === 'Team' &&
+        <div className='chat-room-header__members'>
+          <img src={PersonImage} alt='Person' />
+          <img src={PersonImage} alt='Person' />
+          <img src={PersonImage} alt='Person' />
+          <span>+24</span>
+        </div>
+      }
       <div className='chat-room-header__settings'>
         <DotsIcons className={'chat-room-header__settings-icon'} />
       </div>
