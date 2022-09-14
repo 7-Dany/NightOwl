199d9388fa0301f95e39a3e9c26bcb00f1de2959
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-14 23:24:00 +0200

    Updating SocketContextReducers to include activeConversation and messages

--- night-owl-frontend/src/Context/SocketContextReducers.ts
+++ night-owl-frontend/src/Context/SocketContextReducers.ts
@@ -1,40 +1,77 @@
 import { Socket } from 'socket.io-client'
 
+type TActiveConversation = {
+  conversation_id: string
+  name: string
+  type: string
+  user_id: string
+  username: string
+  image: string
+}
+
+export type TConversationMessage = {
+  message_id: string
+  text: string
+  created_at: string
+  media_url: null | string
+  conversation_id: string
+  user_id: string
+  username: string
+  image: string
+}
+
 export type TSocketContextState = {
   socket: Socket | undefined
-  user_id: string
-  users: string[]
+  users: string[],
+  activeConversation: TActiveConversation,
+  messages: TConversationMessage[]
+}
+
+
+export type TSocketContextTypes =
+  'update_socket'
+  | 'update_users'
+  | 'remove_user'
+  | 'update_active_conversation'
+  | 'update_messages'
+  | 'add_new_message'
+
+export type TSocketContextPayload =
+  string
+  | string[]
+  | Socket
+  | TConversationMessage[]
+  | TActiveConversation
+  | TConversationMessage
+
+export type TSocketContextActions = {
+  type: TSocketContextTypes,
+  payload: TSocketContextPayload
 }
 
 export const defaultSocketContextState: TSocketContextState = {
   socket: undefined,
-  user_id: '',
-  users: []
+  users: [],
+  activeConversation: {} as TActiveConversation,
+  messages: []
 }
 
-export type TSocketContextActions = 'update_socket' | 'update_user_id' | 'update_users' | 'remove_user'
-
-export type TSocketContextPayload = string | string[] | Socket
-
-export type ISocketContextActions = {
-  type: TSocketContextActions,
-  payload: TSocketContextPayload
-}
-
-export function SocketReducer(state: TSocketContextState, action: ISocketContextActions) {
+export function SocketReducer(state: TSocketContextState, action: TSocketContextActions): TSocketContextState {
   console.log(`Message received - Action: ${action.type} - Payload:`, action.payload)
   switch (action.type) {
     case 'update_socket':
       return { ...state, socket: action.payload as Socket }
     case 'update_users':
       return { ...state, users: action.payload as string[] }
-    case 'update_user_id':
-      return { ...state, user_id: action.payload as string }
     case 'remove_user':
       return { ...state, users: state.users.filter(user_id => user_id !== action.payload as string) }
+    case 'update_active_conversation':
+      return { ...state, activeConversation: action.payload as TActiveConversation }
+    case 'update_messages':
+      return { ...state, messages: action.payload as TConversationMessage[] }
+    case 'add_new_message':
+      return { ...state, messages: [...state.messages, action.payload as TConversationMessage] }
     default:
       return { ...state }
   }
 }
-
-
