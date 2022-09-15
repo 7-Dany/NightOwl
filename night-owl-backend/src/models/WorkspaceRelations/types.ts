c22a4279f2f091197c243a71df713fe8702600a7
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-15 22:26:33 +0200

    created types tp include all workspace relations types

---
+++ night-owl-backend/src/models/WorkspaceRelations/types.ts
@@ -0,0 +1,33 @@
+export interface IWorkspace {
+  id?: string
+  name: string
+}
+
+export interface IMember {
+  id?: string
+  workspace_id: string
+  user_id: string
+  role: string
+}
+
+export interface IRequest {
+  id?: string
+  workspace_id: string
+  user_id: string
+}
+
+export interface IWorkspaceMember extends IMember {
+  username: string
+  email: string
+  image: string
+}
+
+export interface IWorkspaceRequest extends IRequest {
+  name: string
+}
+
+export interface IWorkspaceUserRequest extends IRequest {
+  username: string
+  email: string
+  image: string
+}
