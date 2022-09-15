945c4e6c06dfc54ec64dcfc1f5281f16e416d579
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-15 22:28:31 +0200

    updated socket class to include send and create messages

--- night-owl-backend/src/socket.ts
+++ night-owl-backend/src/socket.ts
@@ -2,12 +2,16 @@ import { Server as HTTPServer } from 'http'
 import { Socket, Server } from 'socket.io'
 import { authorizeUser, sessionMiddleware, wrap } from './middlewares'
 import { SessionSocket } from './types'
+import { MessagesModel, IMessage, IUserMessage } from './models'
+
+const messagesModel = new MessagesModel()
 
 export class SocketServer {
   public static instance: SocketServer
   public io: Server
-  /** Master list of all connected users */
-  public workspaces: { [user_id: string]: string }
+
+  /** Master list of all connected users in workspaces */
+  public workspaces: { [workspace_id: string]: string[] }
 
   constructor(server: HTTPServer) {
     SocketServer.instance = this
@@ -34,61 +38,76 @@ export class SocketServer {
     console.log('Socket IO Started')
   }
 
-  StartListeners(defaultSocket: Socket) {
+  StartListeners = (defaultSocket: Socket) => {
     const socket = <SessionSocket>defaultSocket
-    console.log(`Message received from, ${socket.user.id}, ${socket.workspace.workspace_id}`)
+    const { workspace, user } = socket
+    const workspaceId = workspace.workspace_id
+    const userId = user.id
 
-    socket.on('handshake', (callback: (user_id: string, users: string[]) => void) => {
-      console.log(`Handshake received from, ${socket.id}`)
+    /** Setting up handshake event to save workspace if not exist and save user to workspaces as active user */
+    socket.on('handshake', (callback: (users: string[]) => void) => {
+      console.log(`Handshake received from workspace: ${workspaceId}, user: ${userId} }`)
 
       /** Check if this is a reconnection */
-      // const reconnected = Object.values(this.users).includes(socket.id)
-
-      // if (reconnected) {
-      //   console.log(`This user has reconnected, ${socket.id}`)
-      //
-      //   const user_id = this.GetUserIdFromSocketId(socket.id)
-      //   const users = Object.values(this.users)
-      //
-      //   if (user_id) {
-      //     console.log('Sending callback for reconnect...')
-      //     callback(user_id, users)
-      //     return
-      //   }
-      // }
+      const reconnected = this.workspaces[workspaceId]?.includes(userId)
+      if (reconnected) {
+        console.log(`This user has reconnected from workspace: ${workspaceId}, user: ${userId}`)
+        const users = this.workspaces[workspaceId]
+        console.log('Sending callback for reconnect...')
+        callback(users)
+        return
+      }
 
       /** Generating new user */
-      console.log(socket.id)
-      // const users = Object.values(this.users)
+      if (Object.keys(this.workspaces).includes(workspaceId)) {
+        this.workspaces[workspaceId]?.push(userId)
+      } else {
+        this.workspaces[workspaceId] = []
+        this.workspaces[workspaceId]?.push(userId)
+      }
+      callback(this.workspaces[workspaceId])
 
-      console.log('Sending callback for handshake')
-      console.log(this.workspaces)
 
       /** Sending new user to all connected users */
-      // this.SendMessage('user_connected', users.filter(id => id !== socket.id), users)
+      this.SendMessage(
+        'user_connected',
+        this.workspaces[workspaceId]?.filter(id => userId !== id),
+        this.workspaces[workspaceId]
+      )
     })
 
+    /** Setting up get messages event */
+    socket.on('join_conversation', async ({ conversation_id }, callback: (messages: IUserMessage[]) => void) => {
+      console.log(`User:${userId} has joined conversation: ${conversation_id}`)
+      socket.join(conversation_id)
+      const messages = await messagesModel.showByConversation(conversation_id)
+      callback(messages)
+    })
+
+    /** Setting up save message event */
+    socket.on('save_message', async (data: { newMessage: IMessage, conversation: string }, callback) => {
+      console.log(`New message received from: ${userId} its data:`, data)
+      const createdMessage = await messagesModel.create(data.newMessage)
+      socket.to(data.conversation).emit('receive_message', { newMessage: createdMessage })
+      callback(createdMessage)
+    })
+
+    /** After the user get disconnected he will be deleted from active users in workspace */
     socket.on('disconnect', () => {
-      console.log(`Disconnect received from ${socket.id}`)
+      console.log(`Disconnect received from workspace: ${workspaceId}, user: ${userId}`)
+      this.workspaces[workspaceId] = this.workspaces[workspaceId]?.filter(id => userId !== id)
+      this.SendMessage('user_disconnected', this.workspaces[workspaceId], userId)
     })
   }
 
-  // GetUserIdFromSocketId(id: string) {
-  //   return Object.keys(this.users).find(user_id => {
-  //     return this.users[user_id] === id
-  //   })
-  // }
-
   /**
    * Send a message through a socket
-   * @Param name The name of the event.
-   * @Param users List of socket id's.
-   * @Param payload any information needed by the user.
+   * @Param name The name of the event, users List of socket id's, payload any information needed by the user.
    */
-  SendMessage(name: string, users: string[], payload?: Object) {
+  SendMessage = (name: string, users: string[], payload?: Object) => {
     console.log(`Emitting event ${name}, to`, users)
 
     /** Sending Information for all users */
-    users.forEach(id => payload ? this.io.to(id).emit(name, payload) : this.io.to(id).emit(name))
+    users?.forEach(id => payload ? this.io.to(id).emit(name, payload) : this.io.to(id).emit(name))
   }
 }
