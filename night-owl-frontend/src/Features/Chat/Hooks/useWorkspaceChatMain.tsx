36dee35bbdab99296a304d57f1bd645dae9f0e5b
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-16 21:45:37 +0200

    moving WorkspaceChatMain Component hooks to useWorkspaceChatMain hook

---
+++ night-owl-frontend/src/Features/Chat/Hooks/useWorkspaceChatMain.tsx
@@ -0,0 +1,39 @@
+import { useContext, useEffect, useState } from 'react'
+import { AuthContext } from '../../../Context/AuthContext'
+import { SocketContext } from '../../../Context/SocketContext'
+import { IPrivateConversation } from '../../../Types'
+import { getAllUserConversation } from '../Api/conversation_members.api'
+
+function useWorkspaceChatMain() {
+  const { user } = useContext(AuthContext)
+  const { SocketState, SocketDispatch } = useContext(SocketContext)
+  const { activeConversation } = SocketState
+  const [userConversations, setUserConversations] = useState<IPrivateConversation[]>([])
+
+  useEffect(() => {
+    const controller = new AbortController()
+    getAllUserConversation({
+      controller,
+      values: { user_id: user.id, token: user.token }
+    })
+      .then(data => {
+        setUserConversations(data)
+        if (data && !activeConversation?.conversation_id) {
+          SocketDispatch({ type: 'update_active_conversation', payload: data[0] })
+        }
+      })
+      .catch(error => {
+        setUserConversations([])
+      })
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
+  return {
+    activeConversation,
+    userConversations
+  }
+}
+
+export default useWorkspaceChatMain
