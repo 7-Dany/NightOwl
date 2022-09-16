dff1df98eae7d205ebfe08fceaed93a9e4ff93d8
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-16 21:35:37 +0200

    updated message not to show image and username if its for same user

--- night-owl-frontend/src/Features/Chat/Components/Message.tsx
+++ night-owl-frontend/src/Features/Chat/Components/Message.tsx
@@ -1,18 +1,42 @@
+import { useState } from 'react'
+
 type MessageProps = {
   className: string
-  userImage: string
-  userName: string
-  messageDate: string
+  userImage: string | null
+  userName: string | null
+  messageDate: string | null
   messageContent: string
 }
 
-function Message({ className, userImage, userName, messageDate, messageContent }: MessageProps) {
+function Message(
+  {
+    className,
+    userImage,
+    userName,
+    messageDate,
+    messageContent
+  }: MessageProps) {
+  const [showDate, setShowDate] = useState(false)
+
+  function handleShowDate() {
+    setShowDate(prevShowDate => !prevShowDate)
+  }
+
   return (
-    <div className={className}>
-      <img src={userImage} alt='Person' />
-      <p className='person-msg-info'>{userName} <span>{messageDate}</span></p>
-      <div className='message'>
-        <p>
+    <div className={`${className} message-container`}>
+      <div className='message-container__person-info'>
+        {userImage &&
+          <img src={userImage} alt='Person' />
+        }
+        {userName &&
+          <p className='person-msg-info'>{userName}</p>
+        }
+      </div>
+      {showDate &&
+        <span className='message-container__message-date'>{messageDate}</span>
+      }
+      <div className='message' onClick={handleShowDate}>
+        <p className='message__content'>
           {messageContent}
         </p>
       </div>
