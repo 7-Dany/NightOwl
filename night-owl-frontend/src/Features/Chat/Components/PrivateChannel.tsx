6f3ae877642bf8ba94c5e02240e5bc0f067dbc06
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-16 21:37:14 +0200

    moved hooks to usePrivateChannel and added active state to show if a user is active or not

--- night-owl-frontend/src/Features/Chat/Components/PrivateChannel.tsx
+++ night-owl-frontend/src/Features/Chat/Components/PrivateChannel.tsx
@@ -1,33 +1,43 @@
-import { Conversation } from '../Types'
-import React, { useContext } from 'react'
-import { ActiveContext } from '../../../Context/active.context'
+import { IPrivateConversation } from '../../../Types'
+import usePrivateChannel from '../Hooks/usePrivateChannel'
 
 
-function PrivateChannel({
-                          name,
-                          type,
-                          user_id,
-                          image,
-                          text,
-                          username,
-                          conversation_id,
-                          created_at
-                        }: Conversation) {
-  const { setActiveConversation } = useContext(ActiveContext)
-
-  function handleOnClick(event: React.MouseEvent<HTMLDivElement>) {
-    setActiveConversation({ conversation_id, user_id, image, name, username, type })
-  }
+function PrivateChannel(
+  {
+    name,
+    type,
+    user_id,
+    image,
+    text,
+    username,
+    conversation_id,
+    created_at
+  }: IPrivateConversation) {
+  const {
+    active,
+    getTime,
+    handleOnClick
+  } = usePrivateChannel({
+    conversation_id,
+    type,
+    user_id,
+    image,
+    name,
+    username,
+    created_at
+  })
+  const time = getTime(created_at)
 
   return (
     <div className='private-channel' onClick={handleOnClick}>
       <div className='private-channel__name-and-image'>
+        <div className={active}></div>
         <img src={image} alt='person' />
         <p className='private-channel__title'>{username}</p>
-        <p className='private-channel__last-msg'>{text}</p>
+        <p className='private-channel__last-msg'>{text.length > 25 ? text.slice(0, 25) + '...' : text}</p>
       </div>
       <div className='private-channel__unseen-msg'>
-        <span className='private-channel__last-msg-date'>{created_at}</span>
+        <span className='private-channel__last-msg-date'>{`${time.getHours()}:${time.getMinutes()}`}</span>
       </div>
     </div>
   )
