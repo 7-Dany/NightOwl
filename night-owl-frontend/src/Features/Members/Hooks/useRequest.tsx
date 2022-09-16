d0c8fbdee15763386c76890ac35c11e27ae36fcc
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-16 21:43:29 +0200

    added comments to explain functions

--- night-owl-frontend/src/Features/Members/Hooks/useRequest.tsx
+++ night-owl-frontend/src/Features/Members/Hooks/useRequest.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useEffect, useState } from 'react'
-import { AuthContext } from '../../../Context/auth.context'
+import { AuthContext } from '../../../Context/AuthContext'
 import { createWorkspaceMember } from '../Api/workspace_members.api'
 import { deleteWorkspaceRequest } from '../Api/workspace_requests.api'
 
@@ -20,12 +20,24 @@ function useRequest({ userId }: UseRequestArgs): UseRequestReturn {
   useEffect(() => {
     const controller = new AbortController()
     if (order === 'Accept') {
+      /** If the admin accepted workspace request it will get accepted then it will get deleted from the requests,
+       *  then he will be workspace member,
+       *  after the user refresh the page he will get redirected to his workspace,
+       *  if the request got deleted by the sender it will show to the admin when he accepts or delete it.
+       */
       createWorkspaceMember({
         controller,
         values: { userId: userId, token: user.token, workspaceId: workspace.workspace_id, role: 'Member' }
       })
         .then(data => {
-          setMsg('Request got Accepted')
+          if (data) {
+            setMsg('Request got Accepted')
+          }
+        })
+        .catch(error => {
+          if (error.response?.status === 409) {
+            setMsg('Request got deleted by the sender')
+          }
         })
     } else if (order === 'Delete') {
       deleteWorkspaceRequest({
@@ -33,7 +45,14 @@ function useRequest({ userId }: UseRequestArgs): UseRequestReturn {
         values: { userId: userId, token: user.token }
       })
         .then(data => {
-          setMsg('Request got Deleted')
+          if (data) {
+            setMsg('Request got Deleted')
+          }
+        })
+        .catch(error => {
+          if (error.response?.status === 409) {
+            setMsg('Request got deleted by the sender')
+          }
         })
     }
     return () => {
@@ -42,6 +61,7 @@ function useRequest({ userId }: UseRequestArgs): UseRequestReturn {
   }, [order])
 
   function requestOrder(event: React.MouseEvent<HTMLButtonElement>, order: string): void {
+    /** Change order to be Accept or Delete to accept or delete requests */
     setOrder(order)
   }
 
