a7d522d6c1cfa22da0fbe5c7728860ae402596bc
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-16 21:31:17 +0200

    updated ChatRoom Component to show all conversation messages  and be able to send new messages

--- night-owl-frontend/src/Features/Chat/Components/ChatRoom.tsx
+++ night-owl-frontend/src/Features/Chat/Components/ChatRoom.tsx
@@ -1,40 +1,57 @@
-import Message from './Message'
-import { SendMsgIcon, AttachmentIcon, SendImageIcon, MicIcon } from '../Assets'
+import { AttachmentIcon, MicIcon, SendImageIcon, SendMsgIcon } from '../Assets'
+import useChatRoom from '../Hooks/useChatRoom'
+
 
 function ChatRoom() {
+  const {
+    messageRef,
+    message,
+    sendBtn,
+    getMessages,
+    sendMessage,
+    setMessage,
+    handleKeyPress
+  } = useChatRoom()
+
+  const chatMessages = getMessages()
+
   return (
     <div className='chat-room'>
       <div className='chat-room-body'>
-        <Message className={'user-msg'} userName={'Ali Ahmed'} userImage={'./images/person.svg'}
-                 messageDate={'4:45pm'}
-                 messageContent={'Hello How are You?'} />
-        <Message className={'others-msg'} userName={'Ali Ahmed'} userImage={'./images/person.svg'}
-                 messageDate={'4:45pm'}
-                 messageContent={'Hello How are You?'} />
-        <Message className={'user-msg'} userName={'Ali Ahmed'} userImage={'./images/person.svg'}
-                 messageDate={'4:45pm'}
-                 messageContent={'Hello How are You?'} />
-        <Message className={'others-msg'} userName={'Ali Ahmed'} userImage={'./images/person.svg'}
-                 messageDate={'4:45pm'}
-                 messageContent={'Hello How are You?'} />
+        {chatMessages}
+        <div ref={messageRef}></div>
       </div>
       <div className='chat-room-footer'>
-        <div className='chat-room-footer__mic-icon-container' role='button' title='send voice'>
+        <button className='chat-room-footer__mic-icon-container' title='send voice'>
           <MicIcon className={'chat-room-footer__mic-icon'} />
-        </div>
-        <div className='chat-room-footer__image-icon-container' role='button' title='send image'>
+        </button>
+        <button className='chat-room-footer__image-icon-container' title='send image'>
           <SendImageIcon className={'chat-room-footer__image-icon'} />
-        </div>
-        <div className='chat-room-footer__attachment-icon-container' role='button' title='send attachment'>
+        </button>
+        <button className='chat-room-footer__attachment-icon-container' title='send attachment'>
           <AttachmentIcon className={'chat-room-footer__attachment-icon'} />
-        </div>
-        <div className='chat-room-footer__send-msg-btn-container' role='button' title='send message'>
-          <SendMsgIcon className={'chat-room-footer__send-msg-btn'} />
-        </div>
+        </button>
         <label htmlFor='msg-field'>
-          <input type='text' className='chat-room-footer__send-msg' id='msg-field' title='write message'
-                 placeholder='write something...' />
+          <input
+            type='text'
+            className='chat-room-footer__send-msg'
+            id='msg-field' title='write message'
+            placeholder='write something...'
+            name='message'
+            value={message}
+            onChange={(event) => setMessage(event.target.value)}
+            onKeyUp={(event) => handleKeyPress(event)}
+          />
         </label>
+        <button
+          className='chat-room-footer__send-msg-btn-container'
+          title='send message'
+          type='submit'
+          onClick={sendMessage}
+          ref={sendBtn}
+        >
+          <SendMsgIcon className={'chat-room-footer__send-msg-btn'} />
+        </button>
       </div>
     </div>
   )
