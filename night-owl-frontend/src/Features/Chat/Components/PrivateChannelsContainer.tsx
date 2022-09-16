909563de8a3aa0fbfcb07c26f3686336251c1d5c
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-16 21:37:36 +0200

    moved hooks to usePrivateChannelContainer

--- night-owl-frontend/src/Features/Chat/Components/PrivateChannelsContainer.tsx
+++ night-owl-frontend/src/Features/Chat/Components/PrivateChannelsContainer.tsx
@@ -1,57 +1,39 @@
 import { ArrowIcon } from '../../../Assets'
-import PrivateChannel from './PrivateChannel'
-import { Conversation } from '../Types'
-import React, { useContext, useState } from 'react'
-import { ActiveContext } from '../../../Context/active.context'
+import { IPrivateConversation } from '../../../Types'
+import usePrivateChannelContainer from '../Hooks/usePrivateChannelContainer'
 
 type PrivateChannelsContainerProps = {
-  userConversations: Conversation[]
+  userConversations: IPrivateConversation[]
 }
 
 function PrivateChannelsContainer({ userConversations }: PrivateChannelsContainerProps) {
-  const [showPrivateChannels, setShowPrivateChannels] = useState(true)
-  const { activeConversation } = useContext(ActiveContext)
-  let conversations
-  if (activeConversation.conversation_id) {
-    let chosenConversation
-    const filteredConversations = userConversations.map(conversation => {
-      if (conversation.conversation_id === activeConversation.conversation_id) {
-        chosenConversation = <PrivateChannel key={conversation.conversation_id} {...conversation} />
-      } else {
-        return <PrivateChannel key={conversation.conversation_id} {...conversation} />
-      }
-    })
-    conversations = [chosenConversation, ...filteredConversations]
-  } else {
-    conversations = userConversations.map(conversation => {
-      return (
-        <PrivateChannel key={conversation.conversation_id} {...conversation} />
-      )
-    })
-  }
+  const {
+    getConversations,
+    showPrivateChannels,
+    handleShowPrivateChannels
+  } = usePrivateChannelContainer()
 
-  function handleShowPrivateChannels(event: React.MouseEvent<HTMLDivElement>) {
-    setShowPrivateChannels(prevShowPrivateChannels => !prevShowPrivateChannels)
-  }
+  const conversations = getConversations(userConversations)
 
   return userConversations ? (
-    <div className='private-channels-container'>
-      <div className='private-channels'>
-        <p className='private-channels__text'>Direct messages</p>
-        <div
-          className={showPrivateChannels ? 'private-channels__icon-container' : 'private-channels__icon-container active'}
-          onClick={handleShowPrivateChannels}
-        >
-          <ArrowIcon className={'private-channels__icon'} />
+      <div className='private-channels-container'>
+        <div className='private-channels'>
+          <p className='private-channels__text'>Direct messages</p>
+          <div
+            className={showPrivateChannels ? 'private-channels__icon-container' : 'private-channels__icon-container active'}
+            onClick={handleShowPrivateChannels}
+          >
+            <ArrowIcon className={'private-channels__icon'} />
+          </div>
         </div>
+        {showPrivateChannels &&
+          <div className='all-private-channels-container'>
+            {conversations}
+          </div>
+        }
       </div>
-      {showPrivateChannels &&
-        <div className='all-private-channels-container'>
-          {conversations}
-        </div>
-      }
-    </div>
-  ) : <div></div>
+    ) :
+    <div></div>
 }
 
 export default PrivateChannelsContainer
