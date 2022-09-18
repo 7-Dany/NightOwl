387e5a66a0087ea1c9d850531bba8a5b73f6d1fb
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-18 22:43:04 +0200

    updated messages table to include message_type

--- night-owl-backend/migrations/sqls/20220825044343-message-table-up.sql
+++ night-owl-backend/migrations/sqls/20220825044343-message-table-up.sql
@@ -4,6 +4,7 @@ CREATE TABLE messages
     text            TEXT,
     media_url       VARCHAR,
     created_at      TIMESTAMP                          NOT NULL,
+    message_type    VARCHAR(50)                        NOT NULL,
     user_id         uuid REFERENCES users (id)         NOT NULL,
     conversation_id uuid REFERENCES conversations (id) NOT NULL
 );
