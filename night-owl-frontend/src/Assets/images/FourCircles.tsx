b45a653b4c40b96cbc3e175ed8078e22077fe3d5
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-19 21:49:31 +0200

    updated fill color

--- night-owl-frontend/src/Assets/images/FourCircles.tsx
+++ night-owl-frontend/src/Assets/images/FourCircles.tsx
@@ -1,7 +1,7 @@
 function FourCircles() {
   return (
     <svg width='20' height='20' viewBox='0 0 20 20' fill='none' xmlns='http://www.w3.org/2000/svg'>
-      <rect width='20' height='20' fill='#040406' />
+      <rect width='20' height='20' fill='#1C1917' />
       <circle cx='5' cy='5' r='1' fill='#D9D9D9' />
       <circle cx='15' cy='5' r='1' fill='#D9D9D9' />
       <circle cx='5' cy='15' r='1' fill='#D9D9D9' />
