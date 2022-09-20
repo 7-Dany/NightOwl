655122f582fabc398f1099cc5a09c18b0f48ba10
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-21 01:52:03 +0200

    added order to hide input field when record starts

--- night-owl-frontend/src/Features/Chat/Components/ChatRoom.tsx
+++ night-owl-frontend/src/Features/Chat/Components/ChatRoom.tsx
@@ -1,16 +1,18 @@
-import { AttachmentIcon, MicIcon, SendMsgIcon } from '../Assets'
+import { AttachmentIcon, MicIcon, SendMsgIcon, StopIcon } from '../Assets'
 import useChatRoom from '../Hooks/useChatRoom'
+import Record from './Record'
 
 function ChatRoom() {
   const {
     messageRef,
     message,
     sendBtn,
+    setMessage,
     getMessages,
     sendMessage,
-    setMessage,
     handleKeyPress,
-    sendRecord
+    order,
+    setOrder
   } = useChatRoom()
 
   const chatMessages = getMessages()
@@ -22,33 +24,35 @@ function ChatRoom() {
         <div ref={messageRef}></div>
       </div>
       <div className='chat-room-footer'>
-        <button className='chat-room-footer__mic-icon-container' title='send voice' onClick={sendRecord}>
-          <MicIcon className={'chat-room-footer__mic-icon'} />
-        </button>
-        <label htmlFor='msg-field'>
-          <input
-            type='text'
-            className='chat-room-footer__send-msg'
-            id='msg-field' title='write message'
-            placeholder='write something...'
-            name='message'
-            value={message}
-            onChange={(event) => setMessage(event.target.value)}
-            onKeyUp={(event) => handleKeyPress(event)}
-          />
-        </label>
-        <button className='chat-room-footer__attachment-icon-container' title='send attachment'>
-          <AttachmentIcon className={'chat-room-footer__attachment-icon'} />
-        </button>
-        <button
-          className='chat-room-footer__send-msg-btn-container'
-          title='send message'
-          type='submit'
-          onClick={sendMessage}
-          ref={sendBtn}
-        >
-          <SendMsgIcon className={'chat-room-footer__send-msg-btn'} />
-        </button>
+        <Record setOrder={setOrder} message={message} sendBtn={sendBtn} order={order} />
+        {!order &&
+          <>
+            <label htmlFor='msg-field' className='chat-room-footer__send-msg-label'>
+              <input
+                type='text'
+                className='chat-room-footer__send-msg'
+                id='msg-field' title='write message'
+                placeholder='write something...'
+                name='message'
+                value={message}
+                onChange={(event) => setMessage(event.target.value)}
+                onKeyUp={(event) => handleKeyPress(event)}
+              />
+            </label>
+            <button className='chat-room-footer__attachment-icon-container' title='send attachment'>
+              <AttachmentIcon className={'chat-room-footer__attachment-icon'} />
+            </button>
+            <button
+              className='chat-room-footer__send-msg-btn-container'
+              title='send message'
+              type='submit'
+              onClick={sendMessage}
+              ref={sendBtn}
+            >
+              <SendMsgIcon className={'chat-room-footer__send-msg-btn'} />
+            </button>
+          </>
+        }
       </div>
     </div>
   )
