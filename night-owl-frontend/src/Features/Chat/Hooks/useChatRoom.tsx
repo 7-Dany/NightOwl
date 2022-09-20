ad4915a2d2e28cdf2ff73999c6da25b10261d068
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-21 01:50:54 +0200

    moving useRecord to Record component

--- night-owl-frontend/src/Features/Chat/Hooks/useChatRoom.tsx
+++ night-owl-frontend/src/Features/Chat/Hooks/useChatRoom.tsx
@@ -2,34 +2,26 @@ import React, { useContext, useEffect, useRef, useState } from 'react'
 import { AuthContext } from '../../../Context/AuthContext'
 import { SocketContext } from '../../../Context/SocketContext'
 import Message from '../Components/Message'
-import useRecorder, { Recorder } from './useRecorder'
 
 type UseChatRoomReturn = {
   messageRef: React.MutableRefObject<null>
   sendBtn: React.MutableRefObject<null>
   message: string
-  recorderState: Recorder
+  order: boolean
+  setOrder: React.Dispatch<React.SetStateAction<boolean>>
   getMessages: () => JSX.Element[]
   sendMessage: (event: React.MouseEvent<HTMLButtonElement>) => void
   setMessage: React.Dispatch<React.SetStateAction<string>>
   handleKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void
-  sendRecord: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 function useChatRoom(): UseChatRoomReturn {
   const { user } = useContext(AuthContext)
   const { SocketState, SocketDispatch } = useContext(SocketContext)
   const [message, setMessage] = useState('')
-  const [order, setOrder] = useState('')
+  const [order, setOrder] = useState(false)
   const messageRef = useRef(null)
   const sendBtn = useRef(null)
-  const {
-    recorderState,
-    startRecording,
-    saveRecording,
-    cancelRecording
-  } = useRecorder()
-
 
   function scrollToBottom() {
     /** Scroll to the bottom of the chat after messages new message get added */
@@ -45,37 +37,18 @@ function useChatRoom(): UseChatRoomReturn {
     }
   }
 
-
   function sendMessage(event: React.MouseEvent<HTMLButtonElement>) {
     /** Reset the message and send prepare the new message to send for the server to get saved */
     setMessage('')
     if (message.length > 0) {
       SocketDispatch({ type: 'send_message', payload: { data: message, type: 'text' } })
-    } else if (order === 'record') {
-      saveRecording()
     }
-  }
-
-  function sendRecord(event: React.MouseEvent<HTMLButtonElement>) {
-    setOrder('record')
-    setMessage('')
-    startRecording()
   }
 
   useEffect(() => {
     /** Whenever messages get changed the chat room will scroll to bottom */
     scrollToBottom()
   }, [SocketState.messages])
-
-  useEffect(() => {
-    if (recorderState.audio) {
-      SocketDispatch({ type: 'send_message', payload: { data: recorderState.audio, type: 'voice' } })
-      setOrder('')
-      cancelRecording()
-    } else if (message.length > 0) {
-      cancelRecording()
-    }
-  }, [recorderState.audio, message])
 
   function getMessages() {
     /** Setting up messages to show user image and username when the user itself change */
@@ -106,12 +79,12 @@ function useChatRoom(): UseChatRoomReturn {
     messageRef,
     sendBtn,
     message,
-    recorderState,
+    order,
     getMessages,
     sendMessage,
     setMessage,
     handleKeyPress,
-    sendRecord
+    setOrder
   }
 }
 
