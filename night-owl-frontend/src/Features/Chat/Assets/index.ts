b6f42321219d6482d942b1b589e302091f4d3192
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-21 01:52:23 +0200

    added StopIcon for export

--- night-owl-frontend/src/Features/Chat/Assets/index.ts
+++ night-owl-frontend/src/Features/Chat/Assets/index.ts
@@ -2,5 +2,6 @@ import MicIcon from './images/MicIcon'
 import SendImageIcon from './images/SendImageIcon'
 import AttachmentIcon from './images/AttachmentIcon'
 import SendMsgIcon from './images/SendMsgIcon'
+import StopIcon from './images/StopIcon'
 
-export { MicIcon, SendImageIcon, SendMsgIcon, AttachmentIcon }
+export { MicIcon, SendImageIcon, SendMsgIcon, AttachmentIcon, StopIcon }
