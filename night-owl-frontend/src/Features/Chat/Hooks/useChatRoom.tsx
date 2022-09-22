c39964d92eb481695e98ee1fcf3f01a027783ab0
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-22 10:25:45 +0200

    updated useChatRoom hook to send images in the chat

--- night-owl-frontend/src/Features/Chat/Hooks/useChatRoom.tsx
+++ night-owl-frontend/src/Features/Chat/Hooks/useChatRoom.tsx
@@ -4,15 +4,19 @@ import { SocketContext } from '../../../Context/SocketContext'
 import Message from '../Components/Message'
 
 type UseChatRoomReturn = {
-  messageRef: React.MutableRefObject<null>
-  sendBtn: React.MutableRefObject<null>
+  messageRef: React.RefObject<HTMLDivElement>
+  sendBtn: React.RefObject<HTMLButtonElement>
+  fileBtn: React.RefObject<HTMLInputElement>
   message: string
   order: boolean
-  setOrder: React.Dispatch<React.SetStateAction<boolean>>
+  sendFile: boolean
   getMessages: () => JSX.Element[]
-  sendMessage: (event: React.MouseEvent<HTMLButtonElement>) => void
+  setOrder: React.Dispatch<React.SetStateAction<boolean>>
   setMessage: React.Dispatch<React.SetStateAction<string>>
+  setSendFile: React.Dispatch<React.SetStateAction<boolean>>
+  sendMessage: (event: React.MouseEvent<HTMLButtonElement>) => void
   handleKeyPress: (event: React.KeyboardEvent<HTMLInputElement>) => void
+  activeFileButton: (event: React.MouseEvent<HTMLButtonElement>) => void
 }
 
 function useChatRoom(): UseChatRoomReturn {
@@ -20,29 +24,35 @@ function useChatRoom(): UseChatRoomReturn {
   const { SocketState, SocketDispatch } = useContext(SocketContext)
   const [message, setMessage] = useState('')
   const [order, setOrder] = useState(false)
-  const messageRef = useRef(null)
-  const sendBtn = useRef(null)
+  const [sendFile, setSendFile] = useState(false)
+  const messageRef = useRef<HTMLDivElement>(null)
+  const sendBtn = useRef<HTMLButtonElement>(null)
+  const fileBtn = useRef<HTMLInputElement>(null)
 
   function scrollToBottom() {
     /** Scroll to the bottom of the chat after messages new message get added */
-    // @ts-ignore
-    messageRef.current.scrollIntoView({ behavior: 'smooth' })
+    messageRef.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
   function handleKeyPress(event: React.KeyboardEvent<HTMLInputElement>) {
     /** If the user pressed Enter key while writing the message it will send the new message */
     if ((event).key === 'Enter') {
-      // @ts-ignore
-      sendBtn.current.click()
+      sendBtn.current?.click()
     }
   }
 
   function sendMessage(event: React.MouseEvent<HTMLButtonElement>) {
     /** Reset the message and send prepare the new message to send for the server to get saved */
     setMessage('')
+    setSendFile(true)
     if (message.length > 0) {
       SocketDispatch({ type: 'send_message', payload: { data: message, type: 'text' } })
     }
+  }
+
+  function activeFileButton(event: React.MouseEvent<HTMLButtonElement>) {
+    /** Click input file when the attachment get clicked */
+    fileBtn.current?.click()
   }
 
   useEffect(() => {
@@ -80,11 +90,15 @@ function useChatRoom(): UseChatRoomReturn {
     sendBtn,
     message,
     order,
+    fileBtn,
+    sendFile,
+    setSendFile,
     getMessages,
     sendMessage,
     setMessage,
     handleKeyPress,
-    setOrder
+    setOrder,
+    activeFileButton
   }
 }
 
