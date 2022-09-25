2dcc299e1f0da194cfc9ee0aaaaed368c8a41df3
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-25 11:22:25 +0200

    added WorkspaceProjectsMain Component

--- night-owl-frontend/src/Features/Projects/index.tsx
+++ night-owl-frontend/src/Features/Projects/index.tsx
@@ -1,5 +1,17 @@
+import Project from './Components/Project'
+import data from './data.json'
+
 function WorkspaceProjectsMain() {
-  return <div></div>
+  const projects = data.data.map(project => {
+    return (
+      <Project key={project.id} id={project.id} image={project.image} title={project.title} summary={project.summary} />
+    )
+  })
+  return (
+    <div className='workplace-projects-main-container'>
+      {projects}
+    </div>
+  )
 }
 
 export default WorkspaceProjectsMain
