292c249bbd1e1e976781ebaf9cd71dd99836faf9
Ali Ahmed <ali.ali.ameen245@gmail.com>
2022-09-25 11:18:48 +0200

    added ProjectsPage

---
+++ night-owl-frontend/src/Pages/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+import Workspace from '../Layout/Workspace'
+import Projects from '../Features/Projects'
+
+function ProjectsPage() {
+  return (
+    <Workspace title={'Projects'}>
+      <Projects />
+    </Workspace>
+  )
+}
+
+export default ProjectsPage
